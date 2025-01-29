221738a68cd5ad383cde0feb6144e4d9f74f4ab8
ChichiP <tomas.torres5876@gmail.com>
2025-01-29 21:38:42 +1100

    Added grid created by user logic and multicolour painting

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 const mainContainer = document.querySelector("#container")
 const rows = document.querySelector(".row")
 const row = document.createElement('div');
+const button = document.querySelector("#set-grid")
+const resetButton = document.querySelector('#reset-button')
 
 
-let createSquares = function(rows){
+let createGrid = function(rows){
 
 
-    
 
     row.className = 'row'   
     row.style.cssText = "flex:1; display: flex; flex-direction:column"
    
-
-
     for(let i = 0; i < (rows - 1); i++){
         mainContainer.appendChild(row)
         mainContainer.appendChild(row.cloneNode(true)); 
     }
 
-    console.log(rows)
+    
     filas = document.getElementsByClassName('row')
     
     Array.from(filas).forEach(element => {
@@ -26,14 +25,42 @@ let createSquares = function(rows){
         for(let i = 0; i < rows; i++){
             const columna = document.createElement('div');
             columna.classList.add('columna')
+            columna.addEventListener('mouseenter', (e) => { //
+                e.target.style.background = randomCol()
+                
+                
+    
+
+
+            })
             element.appendChild(columna)
 
-        }
-
-   
+        }   
     });
-        
+
+
+    
 }
 
-createSquares(20)
 
+function randomCol(){
+
+    return 'rgb(' + Math.floor(Math.random() * 255).toString() + ',' + Math.floor(Math.random() * 255).toString() + ',' + Math.floor(Math.random() * 255).toString() + ')'
+    
+}
+
+
+
+button.addEventListener('click', (e) => {
+    let input = parseInt(prompt('Ingresa numero entre 2 y 99'));
+  
+    mainContainer.innerHTML = ''
+    row.innerHTML = ''
+    createGrid(input)
+
+})
+
+
+
+
+createGrid(16)
